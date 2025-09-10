9fcc7124e1293fd5b48a4a76ae9037d703194c74
nataliebaker-gif <nataliebaker@seattleacademy.org>
2025-09-10 18:27:58 +0000

    Test!

---
+++ Jokes.py
@@ -0,0 +1,4 @@
+name = input("Hi! What is your name? ")
+print(f"Hi {name} nice to meat you! My name is Natalie!")
+food = input ("What is your favorite food? ")
+print(f"Oh my  gosh! {food} is also my favorite food!")
--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,5 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print("I have two dogs and a cat:")
+print("I love tennis and I have been watching the US open!I want to change this ugly color")

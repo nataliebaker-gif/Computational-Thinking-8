e2563c2fd4b86b01f5a69784b6d79e4afbeffed3
nataliebaker-gif <nataliebaker@seattleacademy.org>
2025-11-14 21:33:05 +0000

    Movement_game

--- movement_game.py
+++ movement_game.py
@@ -53,13 +53,44 @@ window.tracer(0)
 # TODO - set your background
 # TODO - set the starting value for your variable 
 
-create_sprite ("Benny", 0,0)
-set_background("")
+s1 = create_sprite ("Benny", 300,0)
+set_background("TennisCourt")
 
+
+tags = 0
+timer = 0
+obstacles = []
+lives = 5
+s2 = create_sprite("tennisball",300,200)
+
+print ("Game Over")
 
 # Section 3: Controls
 # TODO - define your controls
 # TODO - pick keys for each control
+
+
+def move_up ():
+	s1.setheading (90)
+	s1.forward (50)
+
+def move_down ():
+	s1.setheading (270)
+	s1.forward (50)
+
+def move_left ():
+	s1.setheading(180)
+	s1.forward (50)
+	
+def move_right ():
+	s1.setheading (0)
+	s1.forward (50)
+
+window.onkeypress (move_up, "Up")
+window.onkeypress (move_down, "Down")
+window.onkeypress (move_left, "Left")
+window.onkeypress (move_right, "Right")
+
 
 # Section 4: Game Loop
 window.listen()
@@ -67,9 +98,31 @@ timer = 0
 while True:
 	time.sleep(0.1)
 	timer += 1  
+
+
 	 
     
  	# TODO - code for automatic actions
+
+	#Create tennis balls
+	if timer % 10 == 0:
+		y_position = random.randint(-250, 250)
+		s2 = create_sprite("tennisball",300,y_position)
+		s2.setheading(180)
+		obstacles.append(s2)
+
+
+	# Move each tennis ball
+	for s2 in obstacles:
+		s2.forward(10)
+
+		# if you colide lose a life
+		if get_distance(s1,s2)< 50:
+			lives -= 1
+			s2.hideturtle()
+			obstacles.remove(s2)
+
+
 
 
 
@@ -78,8 +131,9 @@ while True:
 
 	window.update()
 
-	# if :
-	# 	break
 	
-
+	# end of game
+	if lives == 0:
+		break
+	
 print("Game Over")
